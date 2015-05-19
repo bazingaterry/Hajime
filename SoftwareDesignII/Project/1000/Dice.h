eae56eb2f6b37e5ae933b975e4f4214ff3349a65
terrychan <terry@iMac.lan>
2015-05-19 21:13:52 +0800

    bug fix
    修复短时间内ThrowDice点数相同的bug
    允许自定义面数

--- SoftwareDesignII/Project/1000/Dice.h
+++ SoftwareDesignII/Project/1000/Dice.h
@@ -8,23 +8,43 @@
 class Dice
 {
 public:
-    Dice(const int plane);
-    int ThrowDice();		//	return a random dice number
+    Dice(const int plane = 0);
+    int ThrowDice();		                  //	return a random dice number
     int getPlane() const;
     bool isValid(const int diceNumber) const;
-    
+    bool setPlane(const int plane);           /*    set the plane, only can be used when the plane has not been set
+                                                    if set successfully it will return true, else false
+                                              */
 private:
     int plane;					            //	the plane of the dice
+    unsigned int seed;
 };
 
-Dice::Dice(const int plane)
+bool Dice::setPlane(const int plane)
 {
-    this->plane = plane;
+    if (this->plane != 0 || plane <= 0)
+    {
+        return false;
+    }
+    else
+    {
+        this->plane = plane;
+        return true;
+    }
+}
+
+Dice::Dice(const int plane) : seed((unsigned)time(NULL))
+{
+    if (plane > 0)
+        this->plane = plane;
+    else
+        this->plane = 0;
 }
 
 int Dice::ThrowDice()
 {
-    srand((unsigned)time(NULL));	//	use time as seed
+    srand(seed);	//	use time as seed
+    seed = rand();
     return rand() % plane + 1;
 }
 
