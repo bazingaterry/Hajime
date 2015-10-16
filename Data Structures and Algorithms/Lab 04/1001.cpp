723d01fc568fee9ea0df65fec42e9e613e81e02f
terrychan <terry@iMac.local>
2015-10-16 15:53:09 +0800

    fix 1001 bug

--- Data Structures and Algorithms/Lab 04/1001.cpp
+++ Data Structures and Algorithms/Lab 04/1001.cpp
@@ -5,24 +5,26 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     int k;
-    cin >> k;
-    while(k--)
+    while (cin >> k && k)
     {
-        int n, m;
-        cin >> n >> m;
-        int array[50];
-        for (int i = 0; i < m; ++i)
+        while (k--)
         {
-            cin >> array[i];
+            int n, m;
+            cin >> n >> m;
+            int array[50];
+            for (int i = 0; i < m; ++i)
+            {
+                cin >> array[i];
+            }
+            sort(array, array + m);
+            int min = array[m - 1] - array[0];
+            for (int i = 0; i <= m - n; ++i)
+            {
+                if (array[i + n - 1] - array[i] < min)
+                    min = array[i + n - 1] - array[i];
+            }
+            cout << min << endl;
         }
-        sort(array, array + m);
-        int min = array[m - 1] - array[0];
-        for (int i = 0; i <= m - n; ++i)
-        {
-            if (array[i + n - 1] - array[i] < min)
-                min = array[i + n - 1] - array[i];
-        }
-        cout << min << endl;
     }
     return 0;
 }
