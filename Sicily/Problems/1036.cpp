4ab57fa0d6ea2a19280bc7686cd7f8557473425e
bazingaterry <531410054@qq.com>
2016-04-03 01:29:09 +0800

    Update problems 1036

--- Sicily/Problems/1036.cpp
+++ Sicily/Problems/1036.cpp
@@ -9,38 +9,20 @@ int main()
 	while (cin >> keyword && keyword != "THEEND" )
 	{
 		string ciphertext;
-		int  k=0;//the k+1 letter in ciphertext
+		int  k = 0; //the k+1 letter in ciphertext
 		cin >> ciphertext;
-		
 		int col = keyword.size();
-		int row = ciphertext.size()/col;
-		
+		int row = ciphertext.size() / col;
 		char mat[100][10];
-				
-		for (int i='A';i<='Z';i++)
-		{
-			for (int j=0;j<col;j++)//the j+1 letter in keyword 
-			{
-				if (keyword[j]==i)
-				{
-					for (int t=0;t<row;t++)//put ciphertext into matrix
-					{
-						mat[t][j]=ciphertext[k++];
-					}
-				}
-			}
-		}
-		
-		for (int i=0;i<row;i++)
-		{
-			for (int j=0;j<col;j++)
-			{
+		for (int i = 'A'; i <= 'Z'; i++)
+			for (int j = 0; j < col; j++) //the j+1 letter in keyword
+				if (keyword[j] == i)
+					for (int t = 0; t < row; t++) //put ciphertext into matrix
+						mat[t][j] = ciphertext[k++];
+		for (int i = 0; i < row; i++)
+			for (int j = 0; j < col; j++)
 				cout << mat[i][j];
-			}
-		}
 		cout << endl;
 	}
-	
-	
 	return 0;
 }
