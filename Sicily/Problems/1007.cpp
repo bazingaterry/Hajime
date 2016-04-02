43083a63b25e07cc282a107e45e80442998e48d3
bazingaterry <531410054@qq.com>
2016-04-03 01:26:00 +0800

    Update Problems 1007

--- Sicily/Problems/1007.cpp
+++ Sicily/Problems/1007.cpp
@@ -1,38 +1,26 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-//toioynnkpheleaigshareconhtomesnlewx
+
 int main()
 {
 	int n;
-	while(1)
+	while(cin >> n && n)
 	{
-		cin >> n;
-		
-		if (n==0) break;
-		
 		char ch[200];
 		cin >> ch;
 		int m=strlen(ch)/n;
-	
-	
 		for (int i=1;i<=n;++i)
 		{
 			for (int j=1;j<=m;j++)
 			{
-				if (j%2!=0) 
-				{
+				if (j&1)
 					cout << ch[(j-1)*n+i-1];
-				}
-				else 
-				{
+				else
 					cout << ch[(j-1)*n+(n-i)];
-				}
-			}	
+			}
 		}
 		cout << endl;
-	
 	}
-	
 	return 0;
 }
