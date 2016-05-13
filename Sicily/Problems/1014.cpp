6880ae47ed7b8cf8524b54e28f8c16435f5bf3dc
bazingaterry <terry@32ph.com>
2016-05-13 22:34:32 +0800

    fix bug

--- Sicily/Problems/1014.cpp
+++ Sicily/Problems/1014.cpp
@@ -14,8 +14,9 @@ int sum(int num, int digit)
     for(int i = 0; i < v.size(); i++)
         result += v[i];
     return result;
+}
 
-}int main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
     for(int i = 1000; i <= 10000; i++)
         if(sum(i, 10) == sum(i, 12) && sum(i, 10) == sum(i, 16))
